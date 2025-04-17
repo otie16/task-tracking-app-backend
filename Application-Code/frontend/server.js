40fd187fcd61b360e539fa4f5409f665ef67d1a3
otie16 <biggieestone@gmail.com>
2025-04-17 13:51:48 +0100

    adjusted the file structure and added jenkinsfile

--- Application-Code/frontend/server.js
+++ Application-Code/frontend/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
+require("dotenv").config();
+
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 
 // Set up PostgreSQL connection
 const pool = new Pool({
-    user: 'otie16',
-    host: 'localhost',
-    database: 'taskdatadb',
-    password: 'mypassword',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 app.use(cors());
